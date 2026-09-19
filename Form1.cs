46113a318295ab77dc0c88aaf38c1eff742c5437
Giovanna <giovannatimoteo2@gmail.com>
2026-09-18 21:45:02 -0300

    up

--- Form1.cs
+++ Form1.cs
@@ -91,5 +91,70 @@ namespace Cadastro_2
             Txt_Sobrenome.Clear();
            
 		}
+
+        private void Lbl_Cadastro_Pessoal_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Btn_Ativar_Click(object sender, EventArgs e)
+        {   
+            Btn_Nome.Enabled = true; //ativando o botão nome
+            Btn_Sobrenome.Enabled = true; //ativando o botão sobrenome
+            Btn_Idade.Enabled = true; //ativando o botão idade
+            btn_Email.Enabled = true; //ativando o botão email  
+            Btn_Bairro.Enabled = true; //ativando o botão bairro
+            Btn_D_Completos.Enabled = true; //ativando o botão dados completos
+            Btn_Deastivar.Enabled = true; //ativando o botão desativar
+            Btn_Ativar.Enabled = true; //  ativando o botão ativar 
+            Btn_Enviar.Enabled = true; //ativando o botão enviar
+            Txt_Bairro.Enabled = true; //ativando o textbox bairro
+            Txt_Celular.Enabled = true; //ativando o textbox celular
+            Txt_Nome.Enabled = true; //ativando o textbox nome
+            Txt_Sobrenome.Enabled = true; //ativando o textbox sobrenome
+            Txt_Idade.Enabled = true; //ativando o textbox idade
+            Txt_Email.Enabled = true; //ativando o textbox email
+            Txt_Celular.Enabled = true; //ativando o textbox celular
+            Lbl_Nome.Enabled = true; //ativando a label nome
+            Lbl_Sobrenome.Enabled = true; //ativando a label sobrenome
+            Lbl_Idade.Enabled = true; //ativando a label idade
+            Lbl_Bairro.Enabled = true; //ativando a label bairro
+            Lbl_Celular.Enabled = true; //ativando a label celular
+            Lbl_Email.Enabled = true; //ativando a label email
+            Lbl_Cadastro_Pessoal.Enabled = true; //ativando a label cadastro pessoal
+            Lbl_Resultado.Enabled = true; //ativando a label resultado
+            Lbl_Conf_dados.Enabled = true; //ativando a label confirmação de dados
+            Lbl_Bairro.Enabled = true; //ativando a label bairro
+        }
+
+        private void Btn_Deastivar_Click(object sender, EventArgs e)
+        {
+            Btn_Nome.Enabled = false; //desativando o botão nome
+            Btn_Sobrenome.Enabled = false; //desativando o botão sobrenome
+            Btn_Idade.Enabled = false; //desativando o botão idade
+            btn_Email.Enabled = false; //desativando o botão email
+            Btn_Bairro.Enabled = false; //desativando o botão bairro
+            Btn_D_Completos.Enabled = false; //desativando o botão dados completos
+            Btn_Deastivar.Enabled = false; //desativando o botão desativar
+            Btn_Ativar.Enabled = false; //desativando o botão ativar
+            Btn_Enviar.Enabled = false; //desativando o botão enviar
+            Txt_Bairro.Enabled = false; //desativando o textbox bairro
+            Txt_Celular.Enabled = false; //desativando o textbox celular
+            Txt_Nome.Enabled = false; //desativando o textbox nome
+            Txt_Sobrenome.Enabled = false; //desativando o textbox sobrenome
+            Txt_Idade.Enabled = false; //desativando o textbox idade
+            Txt_Email.Enabled = false; //desativando o textbox email
+            Txt_Celular.Enabled = false; //desativando o textbox celular
+            Lbl_Nome.Enabled = true; //ativando a label nome
+            Lbl_Sobrenome.Enabled = false; //desativando a label sobrenome
+            Lbl_Idade.Enabled = false; //desativando a label idade
+            Lbl_Bairro.Enabled = false; //desativando a label bairro
+            Lbl_Celular.Enabled = false; //desativando a label celular
+            Lbl_Email.Enabled = false; //desativando a label email
+            Lbl_Cadastro_Pessoal.Enabled = false; //desativando a label cadastro pessoal
+            Lbl_Resultado.Enabled = false; //desativando a label resultado
+            Lbl_Conf_dados.Enabled = false; //desativando a label confirmação de dados
+            Lbl_Bairro.Enabled = false; //desativando a label bairro
+        }
     }
 }
